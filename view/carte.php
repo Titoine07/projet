39c24be1820522b9e85f02b3dbbb2dd62576e72d
titoine <Stagiaire@IMAC-ELAN-014>
2016-03-15 17:00:49 +0100

    créa event 3

---
+++ view/carte.php
@@ -0,0 +1,25 @@
+
+<section id="page-carte" class="vpheight ">
+	<div class="center">
+		
+
+	<div class="cycle-slideshow"
+			data-cycle-fx=carousel
+			data-cycle-carousel-visible=1
+			data-cycle-fx="scrollHorz"
+			data-cycle-timeout="0"
+			data-cycle-prev="#prev"
+			data-cycle-next="#next">
+	
+			<img src="./img/menu1.jpg"/>
+			<img src="./img/menu2.jpg"/>
+	</div>
+
+	<div class="center">
+			<a href=# id="prev">Prev</a> 
+			<a href=# id="next">Next</a>
+	</div>
+		
+	</div>
+
+</section>
