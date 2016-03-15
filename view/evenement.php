bc79ac8cff362f8c35d4cb5d7eaa41d7828951cf
Titoine <Titoine@192.168.0.15>
2016-03-15 19:03:18 +0100

    event issue

--- view/evenement.php
+++ view/evenement.php
@@ -1,25 +1,23 @@
 
-    <!-- ------------------------------------------------- evenement -------------------------------------------------- -->
-    <section  id="event" class="vpheight adjusting">
+<!-- ------------------------------------------------- evenement -------------------------------------------------- -->
+<section  id="event" class="vpheight adjusting">
 
-            <div class="center">
-                    <?php
-                    
-                             $superCtrl = new SuperCtrlDeLaMortQuiTueMemeLesDimanchesEtLesJoursFeries();
-                             $events = $superCtrl->getCurrentEvents();
-                             var_dump($events);
-                             cpt = 0;
-                             foreach ($events as $event) {
-                                 cpt++
-                              echo '<div class="event event-'.cpt.'"><div class="event-contain"><h3>'.$event   </div></div>';
+    <div class="center">
+        <?php
 
-                             }
-                    
-                ?>
+            $superCtrl = new SuperCtrlDeLaMortQuiTueMemeLesDimanchesEtLesJoursFeries();
+            $events = $superCtrl->getCurrentEvents();
+            var_dump($events);
+            $cpt = 0;
+            foreach ($events as $event) {
+                $cpt++;
+             echo '<div class="event event-'.$cpt.'"><div class="event-contain"><h3>'.$event.'</div></div>';
 
-            </div>
+            }
 
+        ?>
 
-            <div class="bottomslide"><a href="#caroussel"><img src="./img/fleche-rougeup.png"/></a></div>
-    </section>
-	
+    </div>
+
+    <div class="bottomslide"><a href="#caroussel"><img src="./img/fleche-rougeup.png"/></a></div>
+</section>
